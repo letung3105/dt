d3577a8f943a8f1ac09511b4d18f750ab3a60d3e
Vo Le Tung <15053897+letung3105@users.noreply.github.com>
2021-06-09 14:37:23 +0700

    scaffolding for LinkedHashMap

---
+++ src/containers/mod.rs
@@ -0,0 +1,5 @@
+//! Container types
+
+mod linked_hash_map;
+
+pub use linked_hash_map::LinkedHashMap;
---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+//! Implementations of common/interesting data structures.
+//!
+//! # Disclaimer
+//!
+//! These are made for researching/educational puposes and are not meant to be used under any
+//! circumstances. These data structures are suboptimal comparing to Rust's standard library
+//! implementations and are proned to errors or may exhibit unexpected behaviors.
+
+// TODO: more docs
+
+#![warn(missing_docs)]
+#![deny(missing_debug_implementations, unsafe_code)]
+
+pub mod containers;
